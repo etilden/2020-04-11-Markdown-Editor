94c9774bd0a3bf2bd046f53eaf8c78b82c726452
Emma Tilden <e.k.tilden@gmail.com>
2020-04-13 20:24:56 -0500

    Updates

---
+++ precache-manifest.070846dabac92c5ae9d35fb6b73cef9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f5f7efae498ea2109c60f05c2e80a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0e3dfb30717c5f865c4",
+    "url": "/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "75f8a15d01d74fb4a576",
+    "url": "/static/css/main.8dcd9b95.chunk.css"
+  },
+  {
+    "revision": "e0e3dfb30717c5f865c4",
+    "url": "/static/js/2.fb1d379a.chunk.js"
+  },
+  {
+    "revision": "0f660965848ae45ebe4794358aaaa4cb",
+    "url": "/static/js/2.fb1d379a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f8a15d01d74fb4a576",
+    "url": "/static/js/main.429ad3dc.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
